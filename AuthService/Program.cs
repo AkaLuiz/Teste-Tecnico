e3eb241d502e1d678646dc5b4b6ecb4d8f5abeae
AkaLuiz <luizcarlosmdea@gmail.com>
2026-06-15 22:59:44 -0300

    chore(auth): Configure o swagger e inicialização da aplicação

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -2,48 +2,36 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<
+    IAuthenticationService,
+    AuthenticationService>();
 
-var authConnectionString = builder.Configuration.GetConnectionString("AuthDb")
-    ?? throw new InvalidOperationException("Connection string 'AuthDb' not found.");
+builder.Services.AddScoped<JwtService>();
+
+var authConnectionString =
+    builder.Configuration.GetConnectionString("AuthDb")
+    ?? throw new InvalidOperationException(
+        "Connection string 'AuthDb' not found.");
 
 builder.Services.AddDbContext<AuthDbContext>(options =>
     options.UseNpgsql(authConnectionString));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
